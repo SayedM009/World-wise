2deb311e8a8d2a3e09b7a9f1d6455a4005c46d7d
Sayed097 <elsayedmoharam1111@gmail.com>
2024-02-21 18:08:37 +0400

    I have made the home page

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -5,6 +5,14 @@ export default function Homepage() {
     <Navlinks />
     <div className={styles.overlay}></div>
     {/*  eslint-disable-next-line react/no-unescaped-entities */}
-    <h1>I'm Home Page</h1>
+    <section className={styles.text}>
+        <div className={styles.visParent}>
+            <p className={`${styles.paragraph} ${styles.up}`}>You travel the world.</p>
+        </div>
+        <div className={styles.visParent}>
+            <p className={`${styles.paragraph} ${styles.left}`}>WorldWise keeps track of your adventures.</p>
+        </div>
+        <p className={styles.info}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse velit pariatur minima quibusdam fuga. Nesciunt reprehenderit, vel facere voluptatem laudantium qui reiciendis excepturi expedita ut unde culpa, cupiditate, possimus nobis.</p>
+    </section>
     </section>
 }
